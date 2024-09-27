00dc2a3236cfeeedf3084ab88b5b53f96f332e4c
Biswajit Swain <biswajitswain552@gmail.com>
2024-09-27 12:32:34 +0530

    till the part commit create App tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
+import "react-native-reanimated";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      
+      {/* <Stack.Screen name="setting/index" options={{ title: "Setting" }} /> */}
+      <Stack.Screen name="(tabs)" options={{headerShown: false}} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
-import { Text, View } from "react-native";
+import { Link, Redirect } from "expo-router";
+import { ActivityIndicator, Alert, Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Redirect href={'/home'} />
   );
 }
